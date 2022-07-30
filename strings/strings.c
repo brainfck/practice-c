6e384dee22aa152346d351126c679c6cf2c4e2b0
brainfck <mykhailo.tsuber@gmail.com>
2022-07-30 22:35:08 +0300

    string compare

--- strings/strings.c
+++ strings/strings.c
@@ -14,11 +14,21 @@ void my_strcpy(char *dest, const char *src) {
   *dest = 0;
 }
 
+int my_strcmp(const char *s1, const char *s2) {
+  for (; *s1; s1++, s2++) {
+    if (*s1 != *s2)
+      return 0;
+  }
+
+  return 1;
+}
+
 int main() {
   /* printf("%i \n", string_length("SOME BIG STRING")); */
   char dst[] = "Unimaginable";
-  char src[] = "Look Here";
+  char src[] = "Look Here\0";
   my_strcpy(dst, src);
-  printf("%s", dst);
+  printf("Dest: %s \nSource: %s \n", dst, src);
+  printf("%d \n", my_strcmp(dst, src));
   return 0;
 }
