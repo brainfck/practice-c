524bd8b42d22c37bbb26c52d77284d063fdeef29
brainfck <mykhailo.tsuber@gmail.com>
2022-07-30 22:24:46 +0300

    string copy

--- strings/strings.c
+++ strings/strings.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
-int string_length(const char *str){
-    const char *p;
-    for(p = str; *p; p++){
-    }
-    return p - str;
+int string_length(const char *str) {
+  const char *p;
+  for (p = str; *p; p++) {
+  }
+  return p - str;
+}
+
+void my_strcpy(char *dest, const char *src) {
+  for (; *src; src++, dest++) {
+    *dest = *src;
+  }
+  *dest = 0;
 }
 
 int main() {
-    printf("%i \n", string_length("SOME BIG STRING"));
-    return 0;
+  /* printf("%i \n", string_length("SOME BIG STRING")); */
+  char dst[] = "Unimaginable";
+  char src[] = "Look Here";
+  my_strcpy(dst, src);
+  printf("%s", dst);
+  return 0;
 }
