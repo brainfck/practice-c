5c83602690f39a21d8382858fb3b5b81fcd9570d
brainfck <mykhailo.tsuber@gmail.com>
2022-08-07 22:36:40 +0300

    string reverse

--- strings/strings.c
+++ strings/strings.c
@@ -23,12 +23,31 @@ int my_strcmp(const char *s1, const char *s2) {
   return 1;
 }
 
+void swap_char(char *str, int i, int j) {
+  char temp = str[i];
+  str[i] = str[j];
+  str[j] = temp;
+}
+
+void reverse(char *str) {
+  int len = string_length(str);
+  int i;
+  for (i = len; i > len / 2; i--) {
+    swap_char(str, i, len - i - 1);
+  }
+}
+
 int main() {
   /* printf("%i \n", string_length("SOME BIG STRING")); */
-  char dst[] = "Unimaginable";
-  char src[] = "Look Here\0";
-  my_strcpy(dst, src);
-  printf("Dest: %s \nSource: %s \n", dst, src);
-  printf("%d \n", my_strcmp(dst, src));
+  /* char dst[] = "Unimaginable"; */
+  /* char src[] = "Look Here\0"; */
+  /* my_strcpy(dst, src); */
+  /* printf("Dest: %s \nSource: %s \n", dst, src); */
+  /* printf("%d \n", my_strcmp(dst, src)); */
+
+  char test[] = "abcdefghijklmnopqrstuvwxyz";
+  printf("%s \n", test);
+  reverse(test);
+  printf("%s \n", test);
   return 0;
 }
